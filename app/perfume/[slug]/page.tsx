e8449cda192c1ef69c82f84c738682f1d72633a6
Jo repossi <jorgerepossi1980@gmail.com>
2025-03-24 21:32:10 -0300

    fix slug page version 2

--- app/perfume/[slug]/page.tsx
+++ app/perfume/[slug]/page.tsx
@@ -3,16 +3,8 @@ import { createSlug } from "@/utils/slugGenerator";
 import PerfumeDetails from "@/components/perfume-detail";
 import { supabase } from "@/lib/supabaseClient";
 
-import type { Metadata } from "next";
-
-type Props = {
-    params: {
-        slug: string;
-    };
-};
-
-export default async function PerfumeDetailPage({ params }: Props) {
-    const slug = params.slug;
+export default async function Page({ params }: { params: { slug: string } }) {
+    const { slug } = params;
     const slugParts = slug.split("_");
     const id = slugParts[slugParts.length - 1];
 
